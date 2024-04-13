57568d70e017089c78dd2785d15fa5f85093cbac
captaincoro11 <121259483+captaincoro11@users.noreply.github.com>
2024-04-13 22:58:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 
 const corsConfig = {
-    origin:"https://socialmediaapp-frontend.vercel.app/",
+    origin:"http://localhost:3000/",
     credential:true,
     methods:["GET","POST","PUT","DELETE"]
 
