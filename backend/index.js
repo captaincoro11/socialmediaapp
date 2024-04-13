4f11dcb5d9732d069336f183d819b06e1e134025
captaincoro11 <121259483+captaincoro11@users.noreply.github.com>
2024-04-14 04:50:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 
 const corsConfig = {
-    origin:"*",
+    origin: "http://localhost:3000"
     credentials:true,
     methods:["GET","POST","PUT","DELETE"]
 
