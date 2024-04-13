f72a8d8f2517b2aebf40990e6b48d187eab31ac7
captaincoro11 <121259483+captaincoro11@users.noreply.github.com>
2024-04-14 04:26:52 +0530

    Update user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -35,6 +35,7 @@ exports.register = async (req, res) => {
         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
         httpOnly: false,
           secure:true,
+          domain:"https://socialmediaapp-frontend.vercel.app/"
       };
   
       res.status(201).cookie("token", token, options).json({
@@ -76,6 +77,7 @@ exports.login =async(req,res)=>{
         res.status(200).cookie("token",token,{expires:new Date(Date.now()+90*24*60*60*1000),
         httpOnly:false,
         secure:true,
+        domain:"https://socialmediaapp-frontend.vercel.app/",
     }).
         json({
             success:true,
