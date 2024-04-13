e4f393432fea7c640790616ca76eeb23a53bf013
captaincoro11 <121259483+captaincoro11@users.noreply.github.com>
2024-04-14 01:00:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const corsConfig = {
 
 app.use(cors(corsConfig));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
